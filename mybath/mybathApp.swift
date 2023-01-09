c6cb63aaa463590523789332e284c68cc3e31179
lRaulMN7 <raultelo@gmail.com>
2023-01-09 22:01:50 +0100

    Initial Commit

---
+++ mybath/mybathApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybathApp.swift
+//  mybath
+//
+//  Created by Raul Telo Sanchez on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct mybathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
